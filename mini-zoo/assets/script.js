6507fd05084440fe18d747eac0c7f5aafc99488a
Sabrina Andrade <sabrina.andrade.leite@gmail.com>
2018-07-26 13:46:48 -0300

    aula 26/07

--- mini-zoo/assets/script.js
+++ mini-zoo/assets/script.js
@@ -1,1 +1,32 @@
+var dropMenu = document.getElementById('drop-menu');
+dropMenu.addEventListener('change',cliqueEfeito);
 
+// cliqueEfeito - isto é uma função
+
+var pegaFoto = document.querySelectorAll('.thumbs-item');
+
+console.log(dropMenu);
+console.log(pegaFoto);
+
+function cliqueEfeito () {
+    pegaFoto.forEach(foto => {
+		var efeito = dropMenu.value;
+		//console.log(efeito);    
+		if (efeito === "sepia"){
+			foto.classList = "thumbs-item";// esse não tem add pois a gente quer resetar para a classe original e nao ficar aculmulando classes,
+			foto.classList.add("sepia"); // com .add ele soma mais uma classe. para testar tira a linha 16 de cada
+		}
+		if (efeito === "grayscale"){
+			foto.classList = "thumbs-item";
+			foto.classList.add("grayscale");
+		}
+		if (efeito === "invert"){
+			foto.classList = "thumbs-item";
+			foto.classList.add("invert");
+		}
+		if (efeito === ""){
+			foto.classList = "thumbs-item";
+		// esse não tem add pois a gente quer resetar para a classe original e nao ficar aculmulando classes, 
+		}
+    });
+}
--- programadoras/assets/script.js
+++ programadoras/assets/script.js
@@ -1,10 +1,30 @@
+var dropMenu = document.getElementById("drop-menu");
+dropMenu.addEventListener("change", carregaProgramadoras); // pegar as mudancas do menu
 
+window.onload = carregaMenu();// window onload serve para depois de carregar toda a pagina, carregar esse.
 
-
-// function carregaMenu(){
-
-// };
+function carregaMenu (){
+	var nome = document.createElement("option");
+	nome.innerHTML = "selecione sede";
+	nome.value = "none";
+	dropMenu.appendChild(nome); // da linha 5 ao 8 serve para criar a opcao "selecione sede" la no menu
+	for (sede in data){
+	var itemMenu = document.createElement("option");
+	itemMenu.value = sede;
+	itemMenu.innerHTML = sede;
+	dropMenu.appendChild(itemMenu);
+		}
+		};
 
 function carregaProgramadoras(){
-
+var sede = dropMenu.value;
+var listaProgramadoras = document.getElementById("exibe-programadoras");
+listaProgramadoras.innerHTML ="";
+for(turma in data[sede]){// quando usa IN nao precisa usar var turma ou var i, pois da estrutura dele, ja sabe que é um var turma ou var i
+	for (i in data[sede][turma] ["students"]){// studants esta entre "" pq é uma string, e é a chave do objeto// sede é o valor do menu, é melhor manter os msmos nomes dos objetos para as variaveis pra nao confundir
+var img = document.createElement("img");
+img.src = data[sede][turma]["students"] [i] ["photo"];
+		listaProgramadoras.appendChild(img);
+		}
+	}
 };
